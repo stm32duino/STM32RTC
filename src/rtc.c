c46f377d8d32a578e3a88b2613d1c0cdded6abcd
Josh <josh.gaby@gmail.com>
2021-01-25 21:51:56 +1300

    Update src/rtc.c
    Co-authored-by: Frederic Pillon <frederic.pillon@st.com>

--- src/rtc.c
+++ src/rtc.c
@@ -736,7 +736,6 @@ void attachSecondsIrqCallback(voidCallbackPtr func)
        this allows to get a wakeup time equal to 1 s if the counter is 0x0 */
   //LL_RTC_WAKEUP_SetAutoReload(RTC, 0);
   //LL_RTC_WAKEUP_SetClock(RTC, LL_RTC_WAKEUPCLOCK_CKSPRE);
-  
   /* Enable wake up counter and wake up interrupt */
   //LL_RTC_WAKEUP_Enable(RTC);
   //LL_RTC_EnableIT_WUT(RTC);
