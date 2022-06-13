a2793ada4195c6700442bd2b255f5123302cfafd
Alexandre Bourdiol <alexandre.bourdiol@st.com>
2022-06-13 17:19:09 +0200

    fix: in configForLowPower(), begin() should be call unconditionnally
    The fact RTC is already configured or not,
    is handled by begin() itself.
    Fixes https://github.com/stm32duino/STM32LowPower/issues/71
    
    Signed-off-by: Alexandre Bourdiol <alexandre.bourdiol@st.com>

--- src/STM32RTC.cpp
+++ src/STM32RTC.cpp
@@ -926,39 +926,36 @@ void STM32RTC::configForLowPower(Source_Clock source)
 #ifdef __HAL_RCC_RTCAPB_CLKAM_ENABLE
   __HAL_RCC_RTCAPB_CLKAM_ENABLE();
 #endif
-  if (!RTC_IsConfigured()) {
+
+  begin();
+
+  if (_clockSource != source) {
+    // Save current config
+    AM_PM period, alarmPeriod = _alarmPeriod;
+    uint32_t subSeconds;
+    uint8_t seconds, minutes, hours, weekDay, day, month, years;
+    uint8_t alarmSeconds, alarmMinutes, alarmHours, alarmDay;
+    Alarm_Match alarmMatch = _alarmMatch;
+
+    alarmDay = _alarmDay;
+    alarmHours = _alarmHours;
+    alarmMinutes = _alarmMinutes;
+    alarmSeconds = _alarmSeconds;
+
+    getDate(&weekDay, &day, &month, &years);
+    getTime(&seconds, &minutes, &hours, &subSeconds, &period);
+
+    end();
     _clockSource = source;
     // Enable RTC
-    begin();
-  } else {
-    if (_clockSource != source) {
-      // Save current config
-      AM_PM period, alarmPeriod = _alarmPeriod;
-      uint32_t subSeconds;
-      uint8_t seconds, minutes, hours, weekDay, day, month, years;
-      uint8_t alarmSeconds, alarmMinutes, alarmHours, alarmDay;
-      Alarm_Match alarmMatch = _alarmMatch;
-
-      alarmDay = _alarmDay;
-      alarmHours = _alarmHours;
-      alarmMinutes = _alarmMinutes;
-      alarmSeconds = _alarmSeconds;
-
-      getDate(&weekDay, &day, &month, &years);
-      getTime(&seconds, &minutes, &hours, &subSeconds, &period);
-
-      end();
-      _clockSource = source;
-      // Enable RTC
-      begin(period);
-      // Restore config
-      setTime(seconds, minutes, hours, subSeconds, period);
-      setDate(weekDay, day, month, years);
-      setAlarmTime(alarmHours, alarmMinutes, alarmSeconds, alarmPeriod);
-      setAlarmDay(alarmDay);
-      if (RTC_IsAlarmSet()) {
-        enableAlarm(alarmMatch);
-      }
+    begin(period);
+    // Restore config
+    setTime(seconds, minutes, hours, subSeconds, period);
+    setDate(weekDay, day, month, years);
+    setAlarmTime(alarmHours, alarmMinutes, alarmSeconds, alarmPeriod);
+    setAlarmDay(alarmDay);
+    if (RTC_IsAlarmSet()) {
+      enableAlarm(alarmMatch);
     }
   }
 #endif
